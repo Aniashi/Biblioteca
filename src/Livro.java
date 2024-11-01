47533288cde74aab85a858e3a5be40fa7898b92a
Aniashi <anielcarvalho01@gmail.com>
2024-11-01 15:38:52 -0300

    Feat: Class Livro

--- src/Livro.java
+++ src/Livro.java
@@ -1,3 +1,28 @@
 public class Livro {
-    
+    private String titulo;
+	private String categoria;
+	
+	Livro(){}
+	Livro(String titulo, String categoria){
+		this.titulo = titulo;
+		this.categoria = categoria;
+	}
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public String getCategoria() {
+		return categoria;
+	}
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+	public void mostrarDados() {
+		System.out.println("Titulo do livro: "+this.titulo);
+		System.out.println("Categoria do livro: "+this.categoria);
+	}
+
 }
+
